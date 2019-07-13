46480c784287a18fb24d000c424cad105491266f
Madi <leangmadi@gmail.com>
2019-07-13 18:24:22 -0400

    scripts updated in include dropdown menu and home alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-console.log("Hello world");
-alert("Greetings traveler! Welcome to Madi Leang's portfolio site. Please use the navigation to find what you seek!");
+if(window.location.href.indexOf("index.html") !== -1)  {
+    alert("Greetings traveler! Welcome to Madi Leang's portfolio site. Please use the navigation to find what you seek!");
+}
+function myFunction () {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
+    
