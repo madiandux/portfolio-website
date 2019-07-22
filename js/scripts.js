38f5ede9723192eef0f7ed7c2a41b2a36998649d
Madi <leangmadi@gmail.com>
2019-07-21 21:14:05 -0400

    linter clean up

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-if(window.location.href.indexOf("index.html") !== -1)  {
-    alert("Greetings traveler! Welcome to Madi Leang's portfolio site. Please use the navigation to find what you seek!");
+if (window.location.href.indexOf('index.html') !== -1) {
+  alert("Greetings traveler! Welcome to Madi Leang's portfolio site. Please use the navigation to find what you seek!")
 }
 function myFunction () {
-  var x = document.getElementById("myNavtoggle");
-  if (x.className === "navtoggle") {
-    x.className += " responsive";
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    (x.className += 'responsive')
   } else {
-    x.className = "navtoggle";
+    (x.className = 'navtoggle')
   }
 }
-    
